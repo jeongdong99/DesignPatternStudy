7edb7d0dc7b0c7b746beec2dab2448fc7188af1d
jeong dong <93365714+jeungdong@users.noreply.github.com>
2023-04-08 21:36:59 +0900

    Add files via upload

---
+++ SingletonPatten/ChocolateController.java
@@ -0,0 +1,16 @@
+package hf.dp.Singleton;
+
+public class ChocolateController {
+    public static void main(String[] args) {
+        MyHWInfo.printInfo();
+        ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+        ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+
+        boiler.fill();
+        boiler2.reportBoilerState();
+        boiler.boil();
+        boiler2.reportBoilerState();
+        boiler.drain();
+        boiler2.reportBoilerState();
+    }
+}
